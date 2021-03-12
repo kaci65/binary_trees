26f637078efb78fcddf3e66f6b44268f7bfa9693
kaci65 <wanjikukarugi@gmail.com>
2021-03-12 21:54:37 +0300

    Edit file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,10 +17,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	new_node->parent = parent;
+	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	new_node->n = value;
-	new_node = parent;
 
 	return (new_node);
 }
