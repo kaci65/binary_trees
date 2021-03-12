a22bb54f1966f4deed2258210b1f8222b69e7566
kaci65 <wanjikukarugi@gmail.com>
2021-03-12 23:12:47 +0300

    Edit file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,22 +11,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t heightL, heightR;
 
-	if (tree == NULL)
+	if ((tree == NULL) || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
 	else
 	{
-		heightL = binary_tree_height(tree->left) + 1;
-		heightR = binary_tree_height(tree->right) + 1;
+		heightL = binary_tree_height(tree->left);
+		heightR = binary_tree_height(tree->right);
 
 		if (heightL > heightR)
 		{
-			return (heightL);
+			return (heightL + 1);
 		}
 		else
 		{
-			return (heightR);
+			return (heightR + 1);
 		}
 	}
 }
