318bf52c9a205d1f5eb7d7b21a8a2e5e0bcd34df
kaci65 <wanjikukarugi@gmail.com>
2021-03-12 21:57:24 +0300

    Edit file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,8 +24,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_node->parent = parent;
 	new_node->n = value;
+	new_node->parent = parent;
 	if (parent->left)
 	{
 		new_node->left = parent->left;
